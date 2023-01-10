3a3b10e579bc922a5e0248f6329f667e4aba4b9d
Adam Camerer <91383782+ajc3xc@users.noreply.github.com>
2023-01-09 21:51:08 -0600

    Add files via upload

---
+++ 2022-SS-303-Lab01-ajc3xc-master/filter.cpp
@@ -0,0 +1,26 @@
+// filter.cpp - Filter commented-out lines from STDIN
+// Written by Nathan Jarus
+// 2015-01-27  Initial program
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+    string buf;
+    char match = '#';
+	cout<<"Please enter the filter character: ";
+	cin>>match;
+
+    while(getline(cin, buf)) 
+    {
+        if(buf[0] != match) 
+        {
+            cout << buf << endl;
+        }
+    }
+
+    return 0;
+}
+
