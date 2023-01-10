f0b22ed4cc9a9795ce8b2865ecc67e792b0ad2e0
Adam Camerer <91383782+ajc3xc@users.noreply.github.com>
2023-01-09 21:51:28 -0600

    Delete Lab-Project1 directory

--- Lab-Project1/command_substitution1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-filename="hello.cpp"
-lowercase="world"
-a=$(echo "$filename" | sed  's/.cpp//')
-b=$(echo "$lowercase" | tr '[:lower:]' '[:upper:]')
-
-echo "$a, $b"
--- Lab-Project1/command_substitution2.sh
+++
@@ -1,4 +0,0 @@
-value1=4.259
-value2=7.384
-result=$(echo "scale=6;$value1/$value2" | bc)
-echo "$result"
